f85f52ba6e1c86b25045b1da0c8e6ed02d7a9601
Jad Wauthier <jadrien.wauthier@gmail.com>
2021-05-21 14:21:59 -0500

    Initial commit

---
+++ lib/Thingie/Core/Category.pm
@@ -0,0 +1,21 @@
+package Thingie::Core::Category;
+
+use strictures 2;
+
+use Moo;
+
+# VERSION
+
+extends 'Thingie::Core::Thing';
+
+1;
+
+# ABSTRACT: Generic thing category
+
+=pod
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
---
+++ lib/Thingie/Core/Thing.pm
@@ -0,0 +1,19 @@
+package Thingie::Core::Thing;
+
+use strictures 2;
+
+use Moo;
+
+# VERSION
+
+1;
+
+# ABSTRACT: Generic thing
+
+=pod
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
